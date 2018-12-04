a14c35dfb3ac6191dc8286d185eab4321dced039
Riley Mathews <riley.mathews@mm-mathewsr.peace.daveramsey.com>
2018-12-04 12:38:25 -0600

    added default title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
