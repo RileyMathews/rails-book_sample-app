ad29c5699abc524420bdb170a13576c6ab6408f0
Riley Mathews <rileymathews80@gmail.com>
2018-12-14 11:01:07 -0600

    added activation tokens to users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index including pagination" do
